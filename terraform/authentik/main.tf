98ba487cefac047127c98016b100ad27f9eeab41
Vince O'Driscoll <odriscoll.vincent@gmail.com>
2026-05-23 17:55:10 +1000

    chore(terraform): update scope

--- terraform/authentik/main.tf
+++ terraform/authentik/main.tf
@@ -18,14 +18,14 @@ data "authentik_certificate_key_pair" "generated" {
 }
 
 data "authentik_property_mapping_provider_scope" "openid" {
-  name = "authentik managed scope - openid"
+  managed = "goauthentik.io/providers/oauth2/scope-openid"
 }
 data "authentik_property_mapping_provider_scope" "email" {
-  name = "authentik managed scope - email"
+  managed = "goauthentik.io/providers/oauth2/scope-email"
 }
 data "authentik_property_mapping_provider_scope" "profile" {
-  name = "authentik managed scope - profile"
+  managed = "goauthentik.io/providers/oauth2/scope-profile"
 }
 data "authentik_property_mapping_provider_scope" "offline_access" {
-  name = "authentik managed scope - offline_access"
+  managed = "goauthentik.io/providers/oauth2/scope-offline_access"
 }
